dbeaed4684c65ff8e7d2daa5472c08488f205235
francoriformato <franco.riformato001@studenti.uniparthenope.it>
2021-05-07 22:41:52 +0200

    Added loginPage (HTML, CSS, JS)

---
+++ js/loginPage.js
@@ -0,0 +1,7 @@
+function showRegistrationFields() {
+    document.getElementById('name').style.display ='block';
+    document.getElementById('surname').style.display ='block';
+    document.getElementById('username').style.display ='block';
+    document.getElementById('register').textContent = "Continue";
+
+}
