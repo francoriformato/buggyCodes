46bfdc3ecee98589bf5422faf3efa8237c86c5e8
francoriformato <franco.riformato001@studenti.uniparthenope.it>
2021-05-13 19:56:15 +0200

    First PWA attempt: actually the page loads also in offline mode thanks to the cache!

--- js/dashboardFunctions.js
+++ js/dashboardFunctions.js
@@ -3,3 +3,28 @@
 function openURL() {
     window.open('https://www.google.it', '_self');
 }
+
+
+/* PWA */
+
+class PWAConfApp {
+
+}
+
+
+window.addEventListener('load', e => {
+    new PWAConfApp();
+    registerSW();
+});
+
+async function registerSW() {
+    if ('serviceWorker' in navigator) {
+        try {
+            await navigator.serviceWorker.register('./sw.js');
+        } catch (e) {
+            alert('ServiceWorker registration failed. Sorry about that.');
+        }
+    } else {
+        document.querySelector('.alert').removeAttribute('hidden');
+    }
+}
---
+++ sw.js
@@ -0,0 +1,62 @@
+/* SERVICE WORKER */
+
+/* 1 Caching and serving static assets */
+const cacheName = 'pwa-conf-v1';
+const staticAssets = [
+    './',
+    './dashboard.html',
+    './profilePage.html',
+    './newsPage.html',
+    './aboutPage.html',
+    './js/dashboardFunctions.js',
+    './css/style.css',
+    './css/aboutPage.css',
+    './css/loginPage.css',
+    './css/newsPage.css'
+];
+
+
+self.addEventListener('install', async event => {
+    console.log('install event')
+});
+
+
+self.addEventListener('fetch', async event => {
+    console.log('fetch event')
+});
+
+
+self.addEventListener('install', async event => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(staticAssets);
+});
+
+
+self.addEventListener('fetch', event => {
+    const req = event.request;
+
+    if (/.*(json)$/.test(req.url)) {
+        event.respondWith(networkFirst(req));
+    } else {
+        event.respondWith(cacheFirst(req));
+    }
+});
+
+
+async function cacheFirst(req) {
+    const cache = await caches.open(cacheName);
+    const cachedResponse = await cache.match(req);
+    return cachedResponse || networkFirst(req);
+}
+
+async function networkFirst(req) {
+    const cache = await caches.open(cacheName);
+    try {
+        const fresh = await fetch(req);
+        cache.put(req, fresh.clone());
+        return fresh;
+    } catch (e) {
+        const cachedResponse = await cache.match(req);
+        return cachedResponse;
+    }
+}
