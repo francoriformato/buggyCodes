7438efba8c722193fb3a006964185770bdec2a16
francoriformato <83810152+francoriformato@users.noreply.github.com>
2021-05-30 18:05:34 +0200

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,1 @@
-module.exports = {
-  mongodb:{
-    dbURI: "mongodb+srv://buggyAdmin:M98PIEaoQaolSfek@buggycluster.bo7zt.mongodb.net/buggyData?retryWrites=true&w=majority"
-  },
-  google:{
-    clientID:"662987229231-tjlhggo4spp8elhfitchatso25htui31.apps.googleusercontent.com",
-    clientSecret:"otXpwc7syZnws4EVQT1c2U3a"
-  }
-};
+HERE ARE STORED THE KEY OF MONGODB AND GOOGLE API.
